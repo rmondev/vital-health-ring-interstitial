a1dc4edfe104b236841cbcb1b9e77cefb398e03a
Riccardo Moncada <rmondev@gmail.com>
2024-08-20 22:14:22 -0400

    added mobile page 10 with carousel slides (w/ video and pics)

---
+++ src/components/LvlsOfWellNessSml.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+// Props: Array of Image Links (define ts type)
+type Props = {
+    media: string[]
+}
+
+// Pass Props to LvlsOfWellness Component
+const LvlsOfWellnessSml: React.FC<Props> = ({ media }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(false); // Reset the fade-in state
+    const timeout = setTimeout(() => {
+      setIsLoaded(true); // Trigger the fade-in effect after a brief delay
+    }, 10); // Adjust the delay as needed
+
+    return () => clearTimeout(timeout); // Clear the timeout if the component unmounts or media changes
+  }, [media]);
+
+  return (
+    
+ 
+       
+    <div className={`flex flex-col gap-2 mb-4 bg-white justify-center items-center${isLoaded ? 'fade-in' : ''}`}>
+        
+
+            <p className='text-black bg-white text-lg text-center font-semibold'>{media[3]}</p>
+            
+
+        
+        
+      <video 
+        key={media[1]} 
+        className="relative rounded-2xl shadow-lg bg-white" 
+        width="400" 
+        preload='auto' 
+        autoPlay 
+        muted
+        loop
+      >
+        <source src={media[1]} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className='flex flex-row w-full justify-center items-center bg-white gap-2'>
+        <div>
+            <Image 
+                alt={'Lvls of Wellness' + media[2]}
+                src={media[2]}  
+                width={200} 
+                height={200} 
+                className='object-cover bg-white'
+            />
+            </div>
+         
+         <div>   
+            <Image 
+                alt={'Lvls of Wellness' + media[0]}
+                src={media[0]}  
+                width={200} 
+                height={200} 
+                className='object-cover bg-white'
+                />
+            </div>
+      </div>
+
+    </div>
+ 
+
+    
+    
+  );
+}
+
+export default LvlsOfWellnessSml;
--- src/components/Page10.tsx
+++ src/components/Page10.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import LvlsOfWellness from './LvlsOfWellness';
+import LvlsOfWellnessSml from './LvlsOfWellnessSml';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Page10 = () => {
   const [isClient, setIsClient] = useState(false);
@@ -11,21 +16,25 @@ const Page10 = () => {
       '/assets/comp1/image1.png',
       '/videos/Comp1Vid.mp4',
       '/assets/comp1/image2.png',
+      'Wakeup Feeling Refreshed',
     ],
     [
       '/assets/comp2/image1.png',
       '/videos/Comp2Vid.mp4',
       '/assets/comp2/image2.png',
+        'Get Active',
     ],
     [
       '/assets/comp3/image1.png',
       '/videos/Comp3Vid.mp4',
       '/assets/comp3/image2.png',
+        'Stay Informed',
     ],
     [
       '/assets/comp4/image1.png',
       '/videos/Comp4Vid.mp4',
       '/assets/comp4/image2.png',
+        'Relax and Recover',
     ],
   ];
 
@@ -39,13 +48,41 @@ const Page10 = () => {
     return <div className="h-screen">Loading...</div>;
   }
 
+  const settings = {
+    arrows: true,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  console.log('Slider settings:', settings); // Debugging line
+
   return (
+
+    
     <>
       {isSmallScreen ? (
-        <p>Small Screen</p>
+        <div className="flex flex-col justify-center items-center bg-white">
+            <p className='text-black bg-white text-xl text-center font-bold pt-2'>Experience New Levels of Wellness</p>
+        <div className="w-full p-2 bg-white">
+        <Slider {...settings}>
+        {mediaLinks.map((media, index) => (
+            <div key={index}>
+            <LvlsOfWellnessSml media={media} />
+            </div>
+        ))}
+        </Slider>
+        </div>
+        </div>
+
+
       ) : (
         <div className="flex flex-col justify-center items-center bg-white">
-          <p className="bg-transparent text-black font-semibold xl:text-5xl lg:text-3xl md:text-xl text-center p-4">
+          <p className="bg-transparent text-black font-semibold xl:text-5xl lg:text-3xl md:text-xl text-center p-12">
             Experience New Levels of Wellness
           </p>
           
@@ -55,9 +92,8 @@ const Page10 = () => {
               (label, index) => (
                 <div key={index} className="flex flex-col justify-center items-center">
                   <button
-                    className={`bg-transparent text-black font-semibold text-lg p-2 ${
-                      selectedComponent === index + 1 ? 'underline' : ''
-                    }`}
+                    className={`bg-transparent text-black font-bold text-xl p-2 ${
+                      selectedComponent === index + 1}`}
                     onClick={() => setSelectedComponent(index + 1)}
                   >
                     {label}
@@ -70,9 +106,10 @@ const Page10 = () => {
             )}
           </div>
 
-          <div className="h-[1px] w-3/4 bg-black" />
+        {/* <div className="h-[2px] w-5/6 bg-black mt-4" /> */}
 
           <div className="bg-transparent flex flex-row mt-20">
+           
             <LvlsOfWellness media={mediaLinks[selectedComponent - 1]} />
           </div>
         </div>
